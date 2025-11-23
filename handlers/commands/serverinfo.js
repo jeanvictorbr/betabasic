d8385c0a5d13d462fb09404b24a6e9b469c63aa5
jeanvictorbr <jeanvictorbr@gmail.com>
2025-11-23 16:26:16 -0300

    Update serverinfo.js

--- handlers/commands/serverinfo.js
+++ handlers/commands/serverinfo.js
@@ -1,7 +1,10 @@
 const generateServerInfoEmbed = require('../../ui/serverInfoEmbed.js');
+// Importa a flag de efêmero (invisível)
+const { EPHEMERAL_FLAG } = require('../../utils/constants');
 
 module.exports = async function(interaction) {
-    await interaction.deferReply(); // Público, todos veem
+    // Adiciona a flag aqui para tornar a resposta privada
+    await interaction.deferReply({ flags: EPHEMERAL_FLAG }); 
 
     try {
         // Garante que o cache de canais e membros esteja atualizado para contagem correta
